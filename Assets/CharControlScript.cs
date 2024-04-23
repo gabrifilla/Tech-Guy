9340505bea489ff83005b852215ebabe7dc6933b
Gabriel Filla Camargo <gabrifillac@gmail.com>
2024-04-23 16:27:37 -0300

    Added an enemy and made him chase the player.

--- Assets/CharControlScript.cs
+++ Assets/CharControlScript.cs
@@ -1,35 +1,76 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
-public class CharControlScript : MonoBehaviour
+
+namespace StarterAssets
 {
-    [Tooltip("Camera")]
-    public Camera cam;
-    [Tooltip("Player")]
-    public NavMeshAgent player;
-    [Tooltip("Player Animator")]
-    public Animator playerAnimator;
-    [Tooltip("Target Destination")]
-    public GameObject targetDest;
-
-    // Update is called once per frame
-    void Update()
+    [RequireComponent(typeof(CharacterController))]
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+    [RequireComponent(typeof(PlayerInput))]
+#endif
+    public class CharControlScript : MonoBehaviour
     {
-        if(Input.GetMouseButtonDown(1)){
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitPoint;
+        [Tooltip("Camera")]
+        public Camera cam;
+        [Tooltip("Player")]
+        public NavMeshAgent player;
+        [Tooltip("Player Animator")]
+        public Animator playerAnimator;
+        [Tooltip("Target Destination")]
+        public GameObject targetDest;
 
-            if(Physics.Raycast(ray, out hitPoint)){
-                targetDest.transform.position = hitPoint.point;
-                player.SetDestination(hitPoint.point);
+        public AudioClip[] FootstepAudioClips;
+        [Range(0, 1)] public float FootstepAudioVolume = 0.5f;
+
+
+        [Header("Movement")]
+        [SerializeField] ParticleSystem clickEffect;
+        [SerializeField] LayerMask clickableLayer;
+
+
+        private CharacterController _controller;
+
+        private void Start()
+        {
+            _controller = GetComponent<CharacterController>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hitPoint;
+
+                if (Physics.Raycast(ray, out hitPoint))
+                {
+                    targetDest.transform.position = hitPoint.point;
+                    player.SetDestination(hitPoint.point);
+                }
+            }
+
+            if (player.velocity != Vector3.zero)
+            {
+                playerAnimator.SetBool("isWalking", true);
+            }
+            else if (player.velocity == Vector3.zero)
+            {
+                playerAnimator.SetBool("isWalking", false);
             }
         }
 
-        if(player.velocity != Vector3.zero){
-            playerAnimator.SetBool("isWalking", true);
-        }
-        else if(player.velocity == Vector3.zero){
-            playerAnimator.SetBool("isWalking", false);
+        private void OnFootstep(AnimationEvent animationEvent)
+        {
+            if (animationEvent.animatorClipInfo.weight > 0.5f)
+            {
+                if (FootstepAudioClips.Length > 0)
+                {
+                    var index = Random.Range(0, FootstepAudioClips.Length);
+                    AudioSource.PlayClipAtPoint(FootstepAudioClips[index], transform.TransformPoint(_controller.center), FootstepAudioVolume);
+                }
+            }
         }
     }
 }
---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    // Enemy
+    public float health;
+
+    // Patrol
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    // Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    // States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        // Check if player in sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkpoint();
+        if (walkPointSet) agent.SetDestination(walkPoint);
+
+        Vector3 distancetoWalkPoint = transform.position - walkPoint;
+
+        // Walkpoint Reached
+        if (distancetoWalkPoint.magnitude < 1f) walkPointSet = false;
+    }
+    private void SearchWalkpoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround)) walkPointSet = true;
+    }
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+    private void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health < 0) Invoke(nameof(DestroyEnemy), 0.5f);
+    }
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+}
